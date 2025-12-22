0c14dfb1ecbf5b1ab112881ca9592c89ecdcc1e6
Johnlutala <jk.lutala@gmail.com>
2025-12-22 07:23:46 +0100

    Mise en place du controller Notifier

--- src/Controller/NotifierController.php
+++ src/Controller/NotifierController.php
@@ -7,14 +7,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\MerchantRepository;
+use App\Repository\UserRepository;
+use App\Repository\EmailRepository;
+use Symfony\Component\HttpFoundation\Request;
 
- #[Route('/notifier', name: 'app_notifier_')]
+#[Route('/notifier', name: 'app_notifier_')]
 final class NotifierController extends AbstractController
 {
     #[Route('', name: 'index')]
     public function index(
-        MerchantRepository $merchantRepository): Response
-    {
+        MerchantRepository $merchantRepository
+    ): Response {
+
         $merchants = $merchantRepository->findBy(['deleted' => false]);
 
         return $this->render('notifier/index.html.twig', [
@@ -22,5 +26,68 @@ final class NotifierController extends AbstractController
         ]);
     }
 
+    #[Route('/envoyer', name: 'envoyer', methods: ['GET', 'POST'])]
+    public function notifier(
+        Request $request,
+        MerchantRepository $merchantRepository,
+        EmailRepository $emailRepository
+    ): Response {
+        $submittedData = null;
 
+        if ($request->isMethod('POST')) {
+
+            $data = $request->request->all();
+
+            $merchantId = $data['merchant'] ?? null;
+            $executionDate = $data['executionDate'] ?? null;
+            $listName = $data['listName'] ?? null;
+            $totalParticipants = $data['totalParticipants'] ?? null;
+            $phoneNumber = $data['phoneNumber'] ?? [];
+            $reasons = $data['reason'] ?? [];
+
+            $failReasons = [];
+            foreach ($phoneNumber as $i => $phoneNumber) {
+                $failReasons[] = [
+                    'phoneNumber' => $phoneNumber,
+                    'reason' => $reasons[$i] ?? null,
+                ];
+            }
+            
+            
+            // récupérer les emails du marchand sélectionné
+            $emailsList = [];
+            if ($merchantId) {
+                $merchant = $merchantRepository->find($merchantId);
+                if ($merchant) {
+                    $emails = $emailRepository->findBy(['merchant' => $merchant, 'deleted' => false]);
+                    foreach ($emails as $e) {
+                        $emailsList[] = $e->getValue();
+                    }
+                }
+            }
+            
+             $merchants = $merchantRepository->findBy(['deleted' => false]);
+            // récupérer le shortcode du marchand sélectionné (si présent)
+             $merchantcode = isset($merchant) && $merchant ? $merchant->getShortcode() : null;
+
+
+            $submittedData = [
+                'shortcode' => $merchantcode,
+                'emails' => $emailsList,
+                'listName' => $listName,
+                'executionDate' => $executionDate,
+                'totalParticipants' => $totalParticipants,
+                'failReasons' => $failReasons,
+                'emails'=>$emailsList,
+
+            ];
+        }
+
+             dd($submittedData);
+
+        return $this->render('notifier/index.html.twig', [
+            'merchants' => $merchants,
+            'submittedData' => $submittedData,
+        ]);
+    }
 }
